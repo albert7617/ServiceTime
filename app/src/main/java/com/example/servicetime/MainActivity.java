725bfa3630150722c05048bfb91f9335f7ca2976
albert7617 <albert860728@gmail.com>
2019-05-25 20:19:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/servicetime/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.servicetime;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+  boolean started = false;
+  long start_time;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Button add = findViewById(R.id.start_stop);
+    Button discard = findViewById(R.id.discard);
+    Button clear = findViewById(R.id.button3);
+    Button copy = findViewById(R.id.button);
+    final TextView textView = findViewById(R.id.textView);
+
+
+    add.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if(started) {
+          long service = System.currentTimeMillis() - start_time;
+          textView.append(service+",\n");
+          started = false;
+        } else {
+          start_time = System.currentTimeMillis();
+          started = true;
+        }
+      }
+    });
+
+    discard.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        started = false;
+      }
+    });
+
+    clear.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        textView.setText("");
+      }
+    });
+
+    copy.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label", textView.getText());
+        clipboard.setPrimaryClip(clip);
+      }
+    });
+  }
+}
